ee72605c7a0e5c5e79c5c59776f62cfdea73acce
KageKishi <lorhengleap88@gmail.com>
2026-01-23 18:19:17 +0700

    removedLoginregister

--- MyOwn/group.js
+++ MyOwn/group.js
@@ -1,18 +1,9 @@
 
 function register(){
-    document.getElementById('LoginRegister').classList.add('hidden');
     document.getElementById('registerForm').classList.remove('hidden');
-    showScreen('registerForm');
-}
-
-function login(){
-    document.getElementById('LoginRegister').classList.add('hidden');
-    document.getElementById('loginForm').classList.remove('hidden');
-    showScreen('loginForm');
-}
-
-function goBack(){
-    document.getElementById('LoginRegister').classList.remove('hidden');
-    document.getElementById('registerForm').classList.add('hidden');
     document.getElementById('loginForm').classList.add('hidden');
+}
+function goBack(){
+    document.getElementById('registerForm').classList.add('hidden');
+    document.getElementById('loginForm').classList.remove('hidden');
 }

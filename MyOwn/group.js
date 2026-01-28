6a2318f89a4a4cae6a1a2033757c4c70641bbafb
KageKishi <lorhengleap88@gmail.com>
2026-01-28 21:23:14 +0700

    index

--- MyOwn/group.js
+++ MyOwn/group.js
@@ -1,9 +1,9 @@
-
-function register(){
+function register() {
     document.getElementById('registerForm').classList.remove('hidden');
     document.getElementById('loginForm').classList.add('hidden');
 }
-function goBack(){
+
+function goBack() {
     document.getElementById('registerForm').classList.add('hidden');
     document.getElementById('loginForm').classList.remove('hidden');
 }
